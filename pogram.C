051b744fdc91a2967d03877d87f59f24fde592f7
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-01-31 16:52:22 +0530

    program.C

---
+++ pogram.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the value");
+scanf("%d",a);
+b=a%2;
+if(0==b)
+printf("the no is even");
+else
+printf("the no is odd");
+getch();
+return
