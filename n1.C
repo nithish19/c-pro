44ac6bd1b913be8ef2b2c52bef45ae65f07d887e
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-02 14:03:01 +0530

    Create n1.C
    n1.C

---
+++ n1.C
@@ -0,0 +1,21 @@
+#include<stdio.>
+void main()
+{
+int c,d,e;
+printf("enter three numbers:");
+scanf("%d %d %d",&ac,&d,&e);
+if(c>d && c>e)
+{
+    printf("%d",c);
+}
+else if(d>c && d>e)
+{
+    printf("%d",d);
+}
+else if(c>a && e>d)
+{
+    printf("%d",e);
+}
+getch();
+return 0;
+}
