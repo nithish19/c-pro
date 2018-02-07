8e7b930ab46742afb67bc04d4d272d9761812662
nithish19 <35688279+nithish19@users.noreply.github.com>
2018-02-07 16:09:25 +0530

    Create 2nithish7.2.18.C

---
+++ 2nithish7.2.18.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<=a;i++)
+{
+if(i%2==0)
+{
+printf("%d",i);
+}
+}
+getch();
+return 0;
+}
